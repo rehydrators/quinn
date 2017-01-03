3c90f0994e82f267b5b9220f8f419994dabf853e
Vien Tang <vientang@gmail.com>
2017-01-02 18:45:38 -0800

    Add Bing snippet results if Quora doesn't have an answer

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path = require('path');
+var request = require('request');
 var quora = require('quora-api');
+var apiKey = require('./api/API_KEYS');
 var app = express();
 var port = 3030;
+var apiKey = apiKey;
+
+var randomizer = function(arr) {
+	return Math.floor(Math.random() * arr.length);
+}
 
 // mongoose.connect('mongodb://localhost:27017/quinn');
 // var db = mongoose.connection;
@@ -19,31 +26,50 @@ app.get('/', (req, res) => {
 });
 
 app.get('/getAnswers', (req, res) => {
-	var input = req.query.question;
+	var input = req.query.question;		
 
-	var question = function(input) {
+	// format input to match Quora's query string requirement
+	var question = function(input) {		
 		var dashedQuestion = input.replace(/[' ']/gi, '-');
 		return dashedQuestion;
 	};
 
-	quora.answer(question(input)).then(answer => {
-		var fullAnswer = function() {
-			if (!!answer.answer0) {
-				var newAnswer = answer.answer0.split('.');
-				newAnswer.pop();
-				if ( !isNaN(parseInt(newAnswer[newAnswer.length - 1])) ) {
-					newAnswer.pop();
+	quora.answer(question(input)).then(answer => {			
+		if (!!answer.answer0) {
+			var answers = answer.answer0.split('.');
+			answers.pop();
+			if ( !isNaN(parseInt(answers[answers.length - 1])) ) {
+				answers.pop();
+			}
+			answers = answers.join('.').replace(/[.]/gi, '.\n\n');
+			res.send(answers + '.');
+		} else {				
+			// search Bing with the input question with 'quora'
+			var searchQuery = `${input} quora`;
+			var bingUrl = `https://api.cognitive.microsoft.com/bing/v5.0/search?q=${searchQuery}`;		
+			var options = {
+				url: bingUrl,
+				method: 'GET',
+				headers: {
+					'Ocp-Apim-Subscription-Key': apiKey
 				}
-				newAnswer = newAnswer.join('.').replace(/[.]/gi, '.\n\n');
-				return newAnswer + '.';
-			} else {
-				return "I don't know, it's hard to say.";		
-				// hit Bing API to use it's NLP
-					// search Bing with the input question with 'quora'
-					// return the answer from Bing				
-			}				
-		};		
-		res.send(fullAnswer());
+			};				
+
+			// hit Bing API to use it's NLP				
+			request(options, function (error, response, body) {
+			  var jsonData = JSON.parse(body);
+			  // console.log(jsonData.webPages);
+			  var snippets = jsonData.webPages.value.map(function(query) {
+			  	return query.snippet;
+			  });
+			  console.log(jsonData);
+			  if (!error && response.statusCode == 200) {	
+					// return the answer from Bing
+					// console.log(snippets);
+					res.send(snippets[randomizer(snippets)]);				    
+			  }
+			})
+		}
 	});
 });
 
